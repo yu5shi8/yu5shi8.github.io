c5df2c40b6f992f476683c88e7724840996da8bf
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-09-02 17:37:19 +0900

    fix button

--- common.js
+++ common.js
@@ -3,23 +3,20 @@
 // 上までスクロールすると、ページトップに戻るボタンはまた非表示になる
 // ページトップに戻るボタンをクリックすると、上にスクロールしながらページトップに戻る
 
-// 【１】 １番上から＋１００pxスクロールしたら、ページトップに戻るボタンを表示する
-// 今何ピクセルの場所にいるかを取得する
 window.addEventListener('scroll', () => {
   let scrollTop = document.scrollingElement.scrollTop;
   const scrollButton = document.getElementById('scroll-button');
-  // もしも３００px以上スクロールした状態だったら、右下のページトップに戻るボタンを表示する
-  if (scrollTop > 260) {
+  if (scrollTop > 140) {
     scrollButton.classList.add('invisible');
     } else {
-    // そうでなければ表示しない
     scrollButton.classList.remove('invisible');
   }
-  console.log(scrollTop);
-  console.log(scrollButton);
 }, false);
 
 // 【２】 ページトップボタンをクリックしたら、スクロールしながらページトップに戻る
-
+document.getElementById('scroll-button').onclick = function(){
+// スクロールしながら1秒でページトップへ戻る
+  
+};
 
 

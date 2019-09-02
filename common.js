fc6d2e7b8cc4cfd37a2a36adc14e1ee2b3ac8c72
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-09-02 16:26:26 +0900

    add button

---
+++ common.js
@@ -0,0 +1,25 @@
+// やりたいこと
+// 上から少しスクロールすると、ページトップに戻るボタンが右下に表示される
+// 上までスクロールすると、ページトップに戻るボタンはまた非表示になる
+// ページトップに戻るボタンをクリックすると、上にスクロールしながらページトップに戻る
+
+// 【１】 １番上から＋１００pxスクロールしたら、ページトップに戻るボタンを表示する
+// 今何ピクセルの場所にいるかを取得する
+window.addEventListener('scroll', () => {
+  let scrollTop = document.scrollingElement.scrollTop;
+  // もしも３００px以上スクロールした状態だったら、右下のページトップに戻るボタンを表示する
+  const scrollButton = document.getElementById('scroll-button');
+  if (scrollTop < 300) {
+    scrollButton.classList.add('invisible');
+  } else {
+    // そうでなければ表示しない
+    scrollButton.classList.remove('invisible');
+  }
+  console.log(scrollTop);
+  console.log(scrollButton);
+}, false);
+
+// 【２】 ページトップボタンをクリックしたら、スクロールしながらページトップに戻る
+
+
+
